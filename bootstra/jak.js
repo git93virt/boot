e76760c3fb444a4a353791bc3ea1424e0a98addd
unknown <rraj96449@gmail.com>
2021-12-13 18:31:11 +0530

    boot

---
+++ bootstra/jak.js
@@ -0,0 +1,10 @@
+console.log("hello");
+
+const hel = document.getElementByClass("hel");
+for (let i = 0; i < con.length; i++) {
+  const element = con[i];
+}
+const i = document.getElementByClass("i");
+console.log("hello world");
+const i=document.getElementById("d")
+console.log(i);
